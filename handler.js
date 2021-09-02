027b86fd33dffed7b6afde8bcc149c5b9e8836ea
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2021-09-02 10:19:58 +0530

    removing unwanted comments

--- handler.js
+++ handler.js
@@ -1,18 +1,20 @@
 'use strict';
 
-module.exports.hello = async (event) => {
+module.exports.helloworld = async (event,context) => {
+  
+  var obj = JSON.stringify(event);
+  const data = event.requestContext;
+  var httpVal=data.http;
+  var sourceIp=httpVal.sourceIp;
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+        message: 'Hello World',
+        sourceIp:sourceIp
       },
       null,
       2
     ),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
