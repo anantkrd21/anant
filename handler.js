d4ee9c593ea93afa347b54bf9c1e327e61272dcb
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2021-09-02 10:39:04 +0530

    adding comments

--- handler.js
+++ handler.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports.helloworld = async (event,context) => {
-  
+  //extract data from event
   var obj = JSON.stringify(event);
   const data = event.requestContext;
   var httpVal=data.http;
+  //gettign user ip 
   var sourceIp=httpVal.sourceIp;
   return {
     statusCode: 200,
