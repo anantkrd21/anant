c63db87b0e1ac65d2b1e098263de3a8ed9809dcc
anantkrd21 <89842393+anantkrd21@users.noreply.github.com>
2021-09-03 08:18:51 +0530

    Handling error with try catch

--- handler.js
+++ handler.js
@@ -1,20 +1,30 @@
 'use strict';
 
 module.exports.helloworldechoip = async (event,context) => {
-  //getting request information from request Context
-  const data = event.requestContext;
-  var httpVal=data.http;
-  //gettign user ip 
-  var sourceIp=httpVal.sourceIp;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hi '+sourceIp+" It's test api",
-        sourceIp:sourceIp
-      },
-      null,
-      2
-    ),
-  };
+  //getting request information from request event
+  try{
+    const data = event.requestContext;
+    var httpVal=data.http;
+    //gettign user ip 
+    var sourceIp=httpVal.sourceIp;
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Hi '+sourceIp+" It's test api",
+          sourceIp:sourceIp
+        }
+      ),
+    };
+  }catch(err){
+    return {
+      statusCode: 400,
+      body: JSON.stringify(
+        {
+          message: "Something went wrong"
+        }
+      ),
+    };
+  }
+  
 };
